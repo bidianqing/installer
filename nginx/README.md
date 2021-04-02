92dfb6a18fd47da712bb4b459dd32307bf078f00
bidianqing <bdq123@outlook.com>
2021-04-02 10:39:55 +0800

    *

---
+++ nginx/README.md
@@ -0,0 +1,5 @@
+# nginx
+
+```
+bash <(curl -s -L https://raw.githubusercontent.com/bidianqing/installer/main/nginx/nginx.sh)
+```
---
+++ nginx/nginx.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cat > /etc/yum.repos.d/nginx.repo <<EOF
+[nginx-stable]
+name=nginx stable repo
+baseurl=http://nginx.org/packages/centos/$releasever/$basearch/
+gpgcheck=1
+enabled=1
+gpgkey=https://nginx.org/keys/nginx_signing.key
+module_hotfixes=true
+EOF
+
+sudo yum install nginx -y
+
+systemctl start nginx
+systemctl enable nginx
