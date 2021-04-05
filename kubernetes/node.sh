b6c49dd40c99016db077762cd74fb3b8d3113953
骑着毛驴去纽约 <bdq123@outlook.com>
2021-04-05 14:28:13 +0800

    Update node.sh

--- kubernetes/node.sh
+++ kubernetes/node.sh
@@ -43,6 +43,9 @@ gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cl
 exclude=kubelet kubeadm kubectl
 EOF
 
+sudo setenforce 0
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
 sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
 
 systemctl start kubelet
