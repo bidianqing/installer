9a10b3186046af38212b561809f774ef726aeb69
骑着毛驴去纽约 <bdq123@outlook.com>
2021-04-07 19:02:46 +0800

    Update nginx.sh

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-cat <<EOF | sudo tee /etc/yum.repos.d/nginx.repo <<EOF
+cat <<EOF | sudo tee /etc/yum.repos.d/nginx.repo
 [nginx-stable]
 name=nginx stable repo
 baseurl=http://nginx.org/packages/centos/$releasever/$basearch/
+gpgcheck=1
 enabled=1
 gpgkey=https://nginx.org/keys/nginx_signing.key
 module_hotfixes=true
