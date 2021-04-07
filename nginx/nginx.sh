62b240124b27550037dddd9688977458273d2807
骑着毛驴去纽约 <bdq123@outlook.com>
2021-04-07 18:55:33 +0800

    Update nginx.sh

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -4,7 +4,6 @@ cat > /etc/yum.repos.d/nginx.repo <<EOF
 [nginx-stable]
 name=nginx stable repo
 baseurl=http://nginx.org/packages/centos/$releasever/$basearch/
-gpgcheck=1
 enabled=1
 gpgkey=https://nginx.org/keys/nginx_signing.key
 module_hotfixes=true
