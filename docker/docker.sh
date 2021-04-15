d04e6420e226d619329294bb985e02c705cb8318
骑着毛驴去纽约 <bdq123@outlook.com>
2021-04-15 16:17:58 +0800

    Update docker.sh

--- docker/docker.sh
+++ docker/docker.sh
@@ -6,7 +6,7 @@ sudo yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
 
-sudo yum install docker-ce docker-ce-cli containerd.io
+sudo yum install docker-ce docker-ce-cli containerd.io -y
 
 sudo systemctl start docker
 sudo systemctl enable docker
