52b0eae8fbedaaf31d05be0b117a9490448a0f9b
骑着毛驴去纽约 <bdq123@outlook.com>
2021-04-15 16:15:43 +0800

    Create docker.sh

---
+++ docker/docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo yum install -y yum-utils
+
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum install docker-ce docker-ce-cli containerd.io
+
+sudo systemctl start docker
+sudo systemctl enable docker
+
+sudo systemctl status docker
