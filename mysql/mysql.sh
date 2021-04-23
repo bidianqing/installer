ee4b8fc62afa0ae7baf4aab6945972a72809f9a3
骑着毛驴去纽约 <bdq123@outlook.com>
2021-04-23 17:05:26 +0800

    Create mysql.sh

---
+++ mysql/mysql.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+curl -R -O https://repo.mysql.com//mysql80-community-release-el7-3.noarch.rpm
+
+sudo yum install mysql80-community-release-el7-3.noarch.rpm -y
+
+sudo yum install mysql-community-server -y
+
+systemctl start mysqld
+systemctl enable mysqld
+systemctl status mysqld
+
+sudo grep 'temporary password' /var/log/mysqld.log
