eec0fa017bf816fe35af4a09aed09c18cd2fd2e0
三大神捕之四 <bidianqing@qq.com>
2024-12-18 15:12:45 +0800

    Update docker.sh

--- docker-ubuntu/docker.sh
+++ docker-ubuntu/docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
